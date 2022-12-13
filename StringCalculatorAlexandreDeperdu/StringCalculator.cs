30ef86b9d1d56a087966c3159cf38aa7f5129930
alkrynax <alex@neoderme.com>
2022-12-13 15:07:17 +0100

    Validation pour x,y,...

--- StringCalculatorAlexandreDeperdu/StringCalculator.cs
+++ StringCalculatorAlexandreDeperdu/StringCalculator.cs
@@ -5,7 +5,7 @@
         public static object Add(string chaine)
         {
             var parties = chaine.Split(',');
-            return int.Parse(parties[0])+ int.Parse(parties[1]);
+            return parties.Select(int.Parse).Sum();
         }
     }
 }
